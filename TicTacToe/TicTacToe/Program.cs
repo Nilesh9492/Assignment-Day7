ce3a20bfe577e3f62d399965096dcd8b8fa62e25
Nilesh9492 <nilesh94041@gmail.com>
2021-10-12 09:26:35 +0530

    first commit

---
+++ TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTacToe Game !");
+            ticTacToe.CreateGameBoard();
+
+        }
+        public static TicTacToeGame ticTacToe = new TicTacToeGame();
+    }
+}
+        
+     
+
+    
+
---
+++ TicTacToe/TicTacToe/TicTacToeGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class TicTacToeGame
+    {
+        public char[] board = new char[10];
+        public void CreateGameBoard()
+        {
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[i] = ' ';
+            }
+            Console.WriteLine(board[0] + " | " + board[1] + " | " + board[2]);
+            Console.WriteLine("_________");
+            Console.WriteLine(board[3] + " | " + board[4] + " | " + board[5]);
+            Console.WriteLine("_________");
+            Console.WriteLine(board[6] + " | " + board[7] + " | " + board[8]);
+        }
+    }
+}
